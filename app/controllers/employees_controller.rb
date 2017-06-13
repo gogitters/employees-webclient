00987c5a68500731cc74dda8dcdc77f6b27649b0
Samantha Morco <samanthamorco@gmail.com>
2017-06-12 20:09:03 -0400

    restful routes and drying

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,10 +1,50 @@
 class EmployeesController < ApplicationController
+  URL = "http://localhost:3001/api/v1/employees"
+  HEADER = { "Accept" => "application/json" }
   def index
-    @employees = Unirest.get("http://localhost:3001/api/v2/employees.json").body
+    @employees = Unirest.get("#{URL}.json").body
     render :index
   end
 
   def show
-    @employee = Unirest.get("http://localhost:3001/api/v2/employees.json").body
+    @employee = Unirest.get("#{URL}/#{params[:id]}.json").body
+    render :show
+  end
+
+  def new
+    render :new
+  end
+
+  def create
+    employee = Unirest.post("#{URL}.json", headers: HEADER,
+         parameters: { first_name: params[:first_name],
+                       last_name: params[:last_name],
+                       email: params[:email] }
+                      ).body
+    if employee["errors"]
+      flash[:warning] = employee["errors"]
+      redirect_to "/employees/new"
+    else
+      redirect_to "/employees/#{employee['id']}"
+    end
+  end
+
+  def edit
+    @employee = Unirest.get("#{URL}/#{params[:id]}.json").body
+    render :edit
+  end
+
+  def update
+    employee = Unirest.patch("#{URL}/#{params[:id]}.json", headers: HEADER,
+         parameters: { first_name: params[:first_name],
+                       last_name: params[:last_name],
+                       email: params[:email] }).body
+    redirect_to "/employees/#{employee['id']}"
+  end
+
+  def destroy
+    employee = Unirest.delete("#{URL}/#{params[:id]}.json", headers: HEADER).body
+    flash[:success] = employee["message"]
+    redirect_to "/employees"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get "/employees", to: 'employees#index'
+  get "/employees/new", to: 'employees#new'
+  get "/employees/:id", to: 'employees#show'
+  post "/employees", to: 'employees#create'
+  get "/employees/:id/edit", to: 'employees#edit'
+  patch "/employees/:id", to: 'employees#update'
+  delete "/employees/:id", to: 'employees#destroy'
 end
